aa703ac4493cb3141c02d1f13e9cff28976dc08f
alex7est <66407825+alex7est@users.noreply.github.com>
2024-03-10 16:00:04 -0500

    Metodos probados

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -12,6 +12,11 @@ public class TestCalculadora {
 		
 		System.out.println("RESULTADO SUMA: "+resultadoSuma);
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
+		
+		calcu.multiplicar(3, 6);
+		calcu.dividir(30, 5);
+		calcu.promediar(7.8, 5.9, 7.3);
+		calcu.mostrarResultado();
 	}
 
 }

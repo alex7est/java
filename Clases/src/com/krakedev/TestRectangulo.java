b0ad82701649f65469dbfebaf07701e2644a4b50
alex7est <66407825+alex7est@users.noreply.github.com>
2024-03-10 18:14:57 -0500

    Metodo calcularPerimetro agregado

--- Clases/src/com/krakedev/TestRectangulo.java
+++ Clases/src/com/krakedev/TestRectangulo.java
@@ -17,6 +17,12 @@ public class TestRectangulo {
 
         System.out.println("Area de r1: " + areaR1);
         System.out.println("Area de r2: " + areaR2);
+        
+        double perimetroR1 = r1.calcularPerimetro();
+        double perimetroR2 = r2.calcularPerimetro();
+        
+        System.out.println("Perimetro de r2: " + perimetroR2);
+        System.out.println("Perimetro de r1: " + perimetroR1);
 	}
 
 }

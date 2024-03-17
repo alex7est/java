2d473081376a26b6b28a5456d138d09d183e2da2
alex7est <66407825+alex7est@users.noreply.github.com>
2024-03-17 00:35:15 -0500

    Ejercicio Cartas

---
+++ Cartas/src/com/entidades/Carta.java
@@ -0,0 +1,42 @@
+package com.entidades;
+
+public class Carta {
+    private Numero numero;
+    private String palo;
+    private String estado;
+
+    public Carta(Numero numero, String palo) {
+        this.numero = numero;
+        this.palo = palo;
+        this.estado = "N";
+    }
+
+    public Numero getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Numero numero) {
+        this.numero = numero;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println(numero.getNumeroCarta() + "-" + palo);
+    }
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+}
+
---
+++ Cartas/src/com/entidades/Random.java
@@ -0,0 +1,8 @@
+package com.entidades;
+
+public class Random {
+    public static int obtenerPosicion() {
+        return (int) (Math.random() * 52); 
+    }
+}
+
